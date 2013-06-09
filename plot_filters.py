4cb7db94d96147dd5ec070b32b74e2b52c2d1c1d
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-09 13:37:27 +0200

    Updated plot_filters example.

--- plot_filters.py
+++ plot_filters.py
@@ -5,16 +5,16 @@ from __future__ import unicode_literals
 import matplotlib.pyplot as plt
 import numpy as np
 
-from dspy.filters import MelFilter
+from dspy.filters import MelFilterBank
 
 
 def main():
     sample_frequency = 44100
     size = 2048
-    f = MelFilter(sample_frequency)
-    f.create_filter(9, 200, size)
+    filter_bank = MelFilterBank(sample_frequency, size)
     frequencies = np.linspace(0, sample_frequency, size)
-    plt.plot(frequencies, f.spectrum)
+    for mel_filter in filter_bank.filters:
+        plt.plot(frequencies, mel_filter.spectrum)
     plt.xlim([0, sample_frequency / 2])
     plt.show()
 
