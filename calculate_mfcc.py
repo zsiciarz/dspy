29711cc4c16c95d1e5f7870029d1214fd91bb989
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-16 22:30:24 +0200

    Comparing MFCC chart with a spectrogram.

--- calculate_mfcc.py
+++ calculate_mfcc.py
@@ -43,15 +43,25 @@ def main():
         frame = x[idx_begin:idx_end]
         features[i, :] = mfcc(frame, sample_frequency, num_features=num_features)
 
-    # 3. plot the calculated features (transposed so frame number/time is on X axis)
+    # 3. plot the spectrogram of the signframe number/al and calculated features
+    plt.figure()
+    plt.subplot(211)
+    plt.specgram(x, NFFT=frame_size, Fs=sample_frequency)
+    plt.xlim([0, dt * size])
+    plt.ylim([0, sample_frequency / 2.0])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Frequency [Hz]')
+    plt.title('Spectrogram')
+    plt.subplot(212)
     x_scale = np.linspace(0, dt * size, num_frames)
     y_scale = np.arange(0, num_features + 1, 1)
     plt.xlim([0, dt * size])
     plt.ylim([0, num_features])
+    # features transposed so time is on the X axis
     plt.pcolormesh(x_scale, y_scale, features.T)
-    plt.title('MFCC chart')
     plt.xlabel('Time [s]')
     plt.ylabel('Feature number')
+    plt.title('MFCC chart')
     plt.show()
 
 
